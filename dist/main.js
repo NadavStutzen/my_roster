8d28b5c7e544da9edd83965a415fdfeb9eec7c4e
NadavStutzen <nadstutzen@gmail.com>
2020-02-12 14:33:41 +0200

    Organized project

--- dist/main.js
+++ dist/main.js
@@ -1,41 +1,35 @@
-// import { response } from "express"
-let currTeam 
-$('#submit').click(function(){
-     let teamName = $('input').val()
-     $.get(`/teams/${teamName}`,function(response){
-        renderer(response)
-        currTeam = response
-     })
-})
+let currTeam;
+$("#submit").click(function() {
+  let teamName = $("input").val();
+  $.get(`/teams/${teamName}`, function(response) {
+    renderer(response);
+    currTeam = response;
+  });
+});
 
-$('#dream').click(function(){
-    $.get('/dreamTeam',function(response){
-        renderer(response)
-        currTeam = response
-    })
-})
+$("#dream").click(function() {
+  $.get("/dreamTeam", function(response) {
+    renderer(response);
+    currTeam = response;
+  });
+});
 
+$("#container").on("click", ".add-dream", function() {
+  const id = $(this)
+    .closest("div")
+    .data().id;
+  addDreamPlayer(currTeam.find(p => p.id === id));
+});
 
-//const addDreamPlayer = function(player){
-    $('#container').on('click','.add-dream',function(){
-        const id = $(this).closest('div').data().id
-       addDreamPlayer(currTeam.find(p => p.id === id))
-    })
+const addDreamPlayer = function(player) {
+  $.post("/roster", player);
+};
 
-
-    const addDreamPlayer = function(player){
-        $.post('/roster',player)
-    }
-    
-
-
-
-
-
-const renderer = function(players){
-    $('#container').empty()
-    const source = $("#player-template").html()
-    const template = Handlebars.compile(source)
-    const newHTML = template({players: players})
-    $('#container').append(newHTML)
-}
+const renderer = function(players) {
+  const source = $("#player-template").html();
+  const template = Handlebars.compile(source);
+  const newHTML = template({ players: players });
+  $("#container")
+    .empty()
+    .append(newHTML);
+};
